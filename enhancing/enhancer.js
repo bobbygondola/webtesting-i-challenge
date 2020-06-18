42234d990b4d03751f4b9c87e64807094d013772
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-18 11:30:04 -0400

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -3,20 +3,41 @@ module.exports = {
   fail,
   repair,
   get,
+  add
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement >= 20) {
+    return { ...item };
+  } else {
+    return { ...item, enhancement: item.enhancement + 1 };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement > 16) {
+    return { ...item, enhancement: item.enhancement - 1 };
+  } else if (item.enhancement >= 15) {
+    return { ...item, durability: item.durability - 10 };
+  } else if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };n   
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability:100 };
 }
 
 function get(item) {
-  return { ...item };
+  if (item.enhancement === 0) {
+    return { ...item };
+  } else if (item.enhancement > 0) {
+    return { ...item, name: `[+${item.enhancement}] ${item.name}` };
+  }
+
+}
+
+//examples
+function add(a,b){
+  return (a + b)
 }
